adbd326f4b2af1e169131b162cbaf27882aa2a3a
Annaguzel <you@example.com>
2020-03-11 13:59:45 +0200

    Not Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from "react";
-
+import React, { Component } from "react";
 // Data
 import authors from "./data";
 
 // Components
 import Sidebar from "./Sidebar";
-import AuthorList from "./AuthorList";
+import AuthorDetail from "./AuthorDetail";
+import AuthorsList from "./AuthorList";
 
-function App() {
-  return (
-    <div id="app" className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
-        </div>
-        <div className="content col-10">
-          <AuthorList authors={authors} />
+class App extends Component {
+  state = {
+    currentAuthor: null
+  };
+  selectAuthor = author => {
+    const newAuthor = author;
+    this.setState({currentAuthor: newAuthor});
+  };
+
+
+  render() {
+    return (
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar />
+          </div>
+          <div className="content col-10">
+            <AuthorsList authors={authors} selectAuthor={this.selectAuthor} />
+            <AuthorDetail authors={authors} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/AuthorList.js
+++ src/AuthorList.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 // Components
@@ -5,7 +6,7 @@ import AuthorCard from "./AuthorCard";
 
 const AuthorList = props => {
   const authorCards = props.authors.map(author => (
-    <AuthorCard key={author.first_name + author.last_name} author={author} />
+    <AuthorCard key={author.first_name + author.last_name} author={author} selectAuthor={props.selectAuthor}/>
   ));
 
   return (
@@ -17,3 +18,4 @@ const AuthorList = props => {
 };
 
 export default AuthorList;
+
